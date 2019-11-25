ccbb6c6a85e25577bb1025596e3fa6003a0a50e7
atha10 <58178208+atha10@users.noreply.github.com>
2019-11-25 22:19:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,197 +1,143 @@
-const { 
-    Client,
-    Attachment
- } = require('discord.js');
 const Discord = require('discord.js');
-const bot = new Client();
-const ytdl = require("ytdl-core");
+const prefix = '/';
+const token = 'NjQ3NzY4ODYzNDMzNDI0OTM2.XdvxaQ.LUYBBK0VusXJhkavPNV207SD0X8';
+const ytdl = require('ytdl-core');
 
-const token = 'NjQ3NzY4ODYzNDMzNDI0OTM2.Xdvw4w.AjcJ4rhTgRhqwsh-60NLGBpZwnk';
-const PREFIX = '/';
-var version = '1.2';
+const client = new Discord.Client();
 const search = require('youtube-search');
 const opts = {
-    maxResults: 25,
+    maxResults: 1,
     key: 'AIzaSyAz_iSE7lyu3ckiuYVYMdG5PJZQHqAYR0c',
     type: 'video'
+};
+
+const queue = new Map();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
+});
+
+client.once('disconnect', () => {
+	console.log('Disconnect!');
+});
+
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const serverQueue = queue.get(message.guild.id);
+
+	if (message.content.startsWith(`${prefix}play`)) {
+		execute(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}skip`)) {
+		skip(message, serverQueue);
+		return;
+	} else if (message.content.startsWith(`${prefix}stop`)) {
+		stop(message, serverQueue);
+		return;
+	} else {
+		message.channel.send('You need to enter a valid command!')
+	}
+});
+
+async function execute(message, serverQueue) {
+	const args = message.content.split(' ');
+
+	const voiceChannel = message.member.voiceChannel;
+	if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+	const permissions = voiceChannel.permissionsFor(message.client.user);
+	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+		return message.channel.send('I need the permissions to join and speak in your voice channel!');
+    }
+    let filter = m => m.author.id === message.author.id;
+    let query = await message.channel.awaitMessages(filter, { max: 1 });
+    let songInfo = await search(query.first().content, opts).catch(err => console.log(err));
+    //const songInfo = await ytdl.getInfo(args[1]);
+    console.log(songInfo);
+
+        const youtubeResults = songInfo.results;
+        const url2 = youtubeResults.map(result => {
+        return result.id
+        });
+        const title2 = youtubeResults.map(result => {
+            return result.title
+            });
+        console.log(url2);
+const song = {
+    title: title2,
+    url: url2,
+}
+    
+ 
+
+	if (!serverQueue) {
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true,
+		};
+
+		queue.set(message.guild.id, queueContruct);
+
+		queueContruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueContruct.connection = connection;
+			play(message.guild, queueContruct.songs[0]);
+		} catch (err) {
+			console.log(err);
+			queue.delete(message.guild.id);
+			return message.channel.send(err);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(`${song.title} has been added to the queue!`);
+	}
+
 }
 
-var servers = {};
-bot.on('ready',() =>{
-    console.log('Music Bot is READY!!!!!' + version);
+function skip(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+	serverQueue.connection.dispatcher.end();
+}
 
-})
+function stop(message, serverQueue) {
+	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
+}
 
-bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
-    switch (args[0]){
-        case 'play':
-               function play(connection, message){
-                      let name = new Discord.RichEmbed()
-                      .setTitle("🎧 Playing Song 🎧")
-                      .setColor("RED")
-                     message.channel.send(name);
-              
-              
-                          var server = servers[message.guild.id];
-                          server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-                         
-                          server.queue.shift();
-                          server.dispatcher.on("end", function(){
-                              if(server.queue[0]){
-                                  play(connection, message);
-                                   
-                              }else {
-                                  connection.disconnect();
-                              }
-                          
-                      })
-                  }
-                  if(!message.member.voiceChannel){
-                    let voice = new Discord.RichEmbed()
-                     .setTitle("❌ You Must be in a Voice Channel!!!")
-                     .setColor("RED")
-                    message.channel.send(voice);
-                    return;
-                    
-                }
-                if(!servers[message.guild.id]) servers[message.guild.id] = {
-                  queue: []
-                } 
-                var server = servers[message.guild.id];
-                server.queue.push(args[1]);
-            
-                
-                if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){         
-                  play(connection, message);
-                  
-            })
-         
-      /* function play(connection, message){
-        let name = new Discord.RichEmbed()
-       // .setTitle("🎧 Playing Song 🎧")
-       // .setColor("RED")
-       //message.channel.send(name);
+function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+    console.log(song);
 
-
-            var server = servers[message.guild.id];
-            server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-           
-            server.queue.shift();
-            server.dispatcher.on("end", function(){
-                if(server.queue[0]){
-                    play(connection, message);
-                     
-                }else {
-                    connection.disconnect();
-                }
-            
-        })
-    }
-        if(!args[1]){
-
-         message.channel.send(' Please Provide Proper Name Or URL');
-         return;
-        }
-        if(!message.member.voiceChannel){
-            let voice = new Discord.RichEmbed()
-             .setTitle("❌ You Must be in a Voice Channel!!!")
-             .setColor("RED")
-            message.channel.send(voice);
-            return;
-            
-        }
-        if(!servers[message.guild.id]) servers[message.guild.id]= {
-            queue: []
-        }
-        var server = servers[message.guild.id];
-        server.queue.push(args[1]);
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+        queue.delete(guild.id);
         
-        if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-          play(connection, message);
-        })*/
-
-        break;
-    case 'skip':
-            var server = servers[message.guild.id];
-        
-            if(server.dispatcher) server.dispatcher.end();
-            let Skipped = new Discord.RichEmbed()
-            .setTitle("✅ Song Is Skipped ✅")
-             .setColor("GREEN") 
-            message.channel.send(Skipped);
-    break;
-    case 'stop':
-            var server = servers[message.guild.id];
-            if(message.guild.voiceConnection){
-                for(var i = server.queue.length -1; i >= 0; i--){
-
-                server.queue.splice(i,1);
-            }
-            server.dispatcher.end();
-            message.channel.send("✔️ Ending The Queue & Leaving The Channel ✔️");
-            console.log('Stopped the Queue');
-                }
-            if(message.guild.connection) message.guild.voiceConnection.disconnect();
-     break;  
-    /* case 'name':
-            function play(connection, sname){
-              //  let name = new Discord.RichEmbed()
-               // .setTitle("🎧 Playing Song 🎧")
-               // .setColor("RED")
-               //message.channel.send(name);
-        
-        
-                    var server = servers[sname.guild.id];
-                    server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-                   
-                    server.queue.shift();
-                    server.dispatcher.on("end", function(){
-                        if(server.queue[0]){
-                            play(connection, sname);
-                             
-                        }else {
-                            connection.disconnect();
-                        }
-                    
-                })
-            }
-               
-        let filter = m => m.author.id === message.author.id;
-        let query =  message.channel.awaitMessages(filter, { max: 1 });
-        let results =  search(query.first().content, opts).catch(err => console.log(err));
-        if(results) {
-            let youtubeResults = results.results;
-            let i  =0;
-            let titles = youtubeResults.map(result => {
-                i++;
-                return i + ") " + result.title;
-            });
-            console.log(titles);
-            message.channel.send({
-                embed: {
-                    title: 'Select which song you want by typing the number',
-                    description: titles.join("\n")
-                }
-            }).catch(err => console.log(err));
-            
-            filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= youtubeResults.length;
-            let collected = await message.channel.awaitMessages(filter, { maxMatches: 1 });
-            let selected = youtubeResults[collected.first().content - 1];
-            let sname = selected.link;
-
-            embed = new Discord.RichEmbed()
-                .setTitle(`🎧 Playing Song 🎧 ${selected.title}`)
-                .setURL(`${selected.link}`)
-                .setDescription(`${selected.description}`)
-                .setThumbnail(`${selected.thumbnails.default.url}`);
-
-            message.channel.send(embed);
-            if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-                play(connection, sname);
-              })
-        }*/
-    }
-
-})
-bot.login(token);
+		return;
+	}
+   //server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url, {filter: "audio"}))
+		.on('end', () => {
+			console.log('Music ended!');
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => {
+			console.error(error);
+		});
+	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
+client.login(token);
