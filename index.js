56e7295e28ba8f2eee4a0a4d3bb78ba078c75168
atha10 <58178208+atha10@users.noreply.github.com>
2019-11-27 22:38:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ function play(guild, song) {
 	}
 	console.log(serverQueue.songs);
 
-	const dispatcher = serverQueue.connection.playStream(ytdl(song.url,[quality: "highestaudio"]))
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url,{quality: "highestaudio"}))
 		.on('end', reason => {
 			if (reason === 'Stream is not generating quickly enough.') 
       console.log('Song ended');
